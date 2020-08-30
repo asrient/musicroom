9346532e7589c230d2d931fac54cac6bea04742d
Asrient <asen182000@hotmail.com>
2020-08-31 01:54:26 +0530

    chenged player to audio from video, added iossafari check

--- musicroom/static/scripts/shared.js
+++ musicroom/static/scripts/shared.js
@@ -42,3 +42,8 @@ class Api {
 }
 
 window.Api = Api;
+
+const ua = window.navigator.userAgent;
+const iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+const webkit = !!ua.match(/WebKit/i);
+const iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
