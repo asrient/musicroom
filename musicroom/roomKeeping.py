82769d7ae6c4dcba0e8b3c7d7252eccf1ed1d09e
Asrient <asen182000@hotmail.com>
2020-09-01 12:28:14 +0530

    fine tuned room keeper

--- musicroom/roomKeeping.py
+++ musicroom/roomKeeping.py
@@ -1,6 +1,6 @@
 import json
 from channels.consumer import AsyncConsumer
-from asgiref.sync import async_to_sync,sync_to_async
+from asgiref.sync import async_to_sync, sync_to_async
 import asyncio
 from musicroom.common import to_json
 from musicroom.models import Room
@@ -19,11 +19,11 @@ def execute_skipto(room_id):
         curr_time = timezone.now()
         if not room.is_paused:
             dur = room.duration_to_complete
-            if curr_time >= (room.play_start_time+datetime.timedelta(seconds=dur.second,  minutes=dur.minute)):
+            if curr_time >= (room.play_start_time+datetime.timedelta(seconds=dur.second,  minutes=dur.minute)-datetime.timedelta(seconds=5,  minutes=0)):
                 print('valid time to skipto')
                 room.skip_to_next()
             else:
-                print('NOT valid time to skipto')
+                print('NOT valid time to skipto',curr_time-(room.play_start_time+datetime.timedelta(seconds=dur.second,  minutes=dur.minute)))
         else:
             print('skipto canceled, playback is paused')
 
@@ -33,7 +33,7 @@ class RoomKeeper(AsyncConsumer):
         if 'room_id' in event and 'timeout' in event:
             room_id = event['room_id']
             timeout = event['timeout']
-            print('scheduling skipto',room_id,timeout)
+            print('scheduling skipto', room_id, timeout)
             await asyncio.sleep(timeout)
-            async_wrapper=sync_to_async(execute_skipto)
+            async_wrapper = sync_to_async(execute_skipto)
             await async_wrapper(room_id)
