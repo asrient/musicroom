88d59d8de2212a5432f1da6aed10bcc858d32563
Asrient <asen182000@hotmail.com>
2020-09-06 19:55:55 +0530

    turn off debug mode

--- musicroom/settings.py
+++ musicroom/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJ_SECRET_KEY', 'bhyr/@5h56yBhgr$t6uj*RF!0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1']
 
