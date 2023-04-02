9290018861deedeac120afc0b1736f6df59a5d6a
asrient <44570278+asrient@users.noreply.github.com>
2023-04-02 19:40:53 +0530

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 cd server
 source venv/bin/activate
-export DATABASE_URL="postgres://mr:123456@127.0.0.1:5432/mr"
+export DATABASE_URL="<postgres://user:xxxx@127.0.0.1:5432/db>"
 python manage.py collectstatic
 python manage.py migrate --run-syncdb
 gunicorn musicroom.wsgi -b 0.0.0.0:8000 
